37c9221ceee0fe39cd1dd49e1efecb37e5d720bf
ddu21 <susutnalsw@swu.ac.kr>
2024-07-28 03:03:48 +0900

    First Commit!!

--- app/src/main/java/com/example/guruapp/LoginFragment.kt
+++ app/src/main/java/com/example/guruapp/LoginFragment.kt
@@ -1,58 +1,59 @@
 package com.example.guruapp
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toolbar
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import java.util.regex.Pattern
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class LoginFragment : AppCompatActivity() {
+    lateinit var btnLogin: Button
+    lateinit var editTextId: EditText
+    lateinit var editTextPassword: EditText
+    lateinit var btnRegister: Button
+    var DB:DBHelper?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setContentView(R.layout.activity_main)
 
-    }
+        DB = DBHelper(this)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
+        btnLogin = findViewById(R.id.btnLogin)
+        editTextId = findViewById(R.id.editTextId)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        btnRegister = findViewById(R.id.btnRegister)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        // 로그인 버튼 클릭
+        btnLogin!!.setOnClickListener {
+            val user = editTextId!!.text.toString()
+            val pass = editTextPassword!!.text.toString()
+
+            // 빈칸 제출시 Toast
+            if (user == "" || pass == "") {
+                Toast.makeText(this@LoginFragment, "아이디와 비밀번호를 모두 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val checkUserpass = DB!!.checkUserpass(user, pass)
+                // id 와 password 일치시
+                if (checkUserpass == true) {
+                    Toast.makeText(this@LoginFragment, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this@LoginFragment, "아이디 또는 비밀번호가 맞지 않아요.", Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+        // 회원가입 버튼 클릭
+        btnRegister.setOnClickListener {
+            val loginIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 }
-
